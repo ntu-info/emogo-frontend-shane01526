b7d598f155d076c563e4fbb411c49bb2a8a7d01e
shane01526 <applegirlmabel@gmail.com>
2025-11-24 16:17:12 +0800

    Fix: Remove Fragment wrapper from root layout - resolves Invariant Violation error

--- app/_layout.js
+++ app/_layout.js
@@ -15,33 +15,30 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <>
-      {/* Root stack controls screen transitions for the whole app */}
-      <Stack>
-        {/* The (tabs) group is one Stack screen with its own tab navigator */}
-        <Stack.Screen
-          name="(tabs)"
-          options={{ headerShown: false }}
-        />
-        {/* This screen is pushed on top of tabs when you navigate to /details */}
-        <Stack.Screen
-          name="details"
-          options={{ title: "Details" }}
-        />
-        {/* Data collection screens */}
-        <Stack.Screen
-          name="questionnaire"
-          options={{ title: "Questionnaire", presentation: "modal" }}
-        />
-        <Stack.Screen
-          name="vlog"
-          options={{ title: "Vlog Recorder", presentation: "modal" }}
-        />
-        <Stack.Screen
-          name="location"
-          options={{ title: "Location Tracker", presentation: "modal" }}
-        />
-      </Stack>
-    </>
+    <Stack>
+      {/* The (tabs) group is one Stack screen with its own tab navigator */}
+      <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false }}
+      />
+      {/* This screen is pushed on top of tabs when you navigate to /details */}
+      <Stack.Screen
+        name="details"
+        options={{ title: "Details" }}
+      />
+      {/* Data collection screens */}
+      <Stack.Screen
+        name="questionnaire"
+        options={{ title: "Questionnaire", presentation: "modal" }}
+      />
+      <Stack.Screen
+        name="vlog"
+        options={{ title: "Vlog Recorder", presentation: "modal" }}
+      />
+      <Stack.Screen
+        name="location"
+        options={{ title: "Location Tracker", presentation: "modal" }}
+      />
+    </Stack>
   );
 }
